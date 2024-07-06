c1bcc721459b8ccc103efc8842d986658eb2bde0
umarhunter <markufaruque@gmail.com>
2024-07-06 12:42:03 -0400

    Create doc_parser.py script that allows for arguments to be passed.

---
+++ doc_parser.py
@@ -0,0 +1,16 @@
+import argparse
+
+def main(path):
+    if path is not None:
+        assert path.endswith('.docx'), 'File must be a Word document (.docx)'
+    else:
+        raise ValueError("No file path provided.")
+
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Gather file names and ')
+    parser.add_argument('--path', type=str, help='File path to a Word (.docx) document')
+    args = parser.parse_args()
+    main(args.path)
